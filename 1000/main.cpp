7ab7525cdf005bdd81145886a58afd7cad6cc960
Alexey Taynov <taynovai@gmail.com>
2017-01-22 22:08:34 +0500

    Задача 1000, решено

---
+++ 1000/main.cpp
@@ -0,0 +1,29 @@
+
+#include <cstdio>
+#include <cmath>
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+
+using namespace std;
+
+int main ()
+{
+    ios_base::sync_with_stdio ();
+
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout << a + b;
+    return 0;
+
+}
