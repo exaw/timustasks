d5e162ba44108bebc22b19273a26dfe21107c464
Alexey Taynov <taynovai@gmail.com>
2017-02-05 22:17:08 +0500

    1264. Трудовые будни. Решение принято

---
+++ 1264/main.cpp
@@ -0,0 +1,30 @@
+
+#include <cstdio>
+#include <cmath>
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+
+
+using namespace std;
+
+int main ()
+{
+    ios_base::sync_with_stdio (false);
+
+    int n=0, m=0;
+    cin>>n>>m;
+
+    int time = n*(m+1);
+    cout<<time<<endl;
+
+    return 0;
+}
