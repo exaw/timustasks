a255b1ee2a8d5622172de7a3b729f22e6935808f
Alexey Taynov <taynovai@gmail.com>
2017-02-05 19:55:52 +0500

    Задача 1293. Эния. Решение принято

---
+++ 1293/main.cpp
@@ -0,0 +1,26 @@
+
+#include <cstdio>
+#include <cmath>
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+
+
+using namespace std;
+
+int main ()
+{
+    ios_base::sync_with_stdio (false);
+
+    int n=0, a=0, b=0;
+    cin>>n>>a>>b;
+    cout<<n*a*b*2;
+}
